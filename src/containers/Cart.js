7944ff23c47e3a04d9314e4404835e5ea7b15feb
franponce <franponce98@gmail.com>
2023-08-24 22:10:38 -0300

    The containers for the different views are added

---
+++ src/containers/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Cart() {
+  return (
+    <div>
+        <h1>Carrito de compras</h1>
+    </div>
+  )
+}
+
---
+++ src/containers/Products.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Products() {
+  return (
+    <div>
+        <h1>Indice de productos</h1>
+    </div>
+  )
+}
