321c17ef74857f275aed61a4bca9958137bcefcb
franponce <franponce98@gmail.com>
2023-08-24 22:14:08 -0300

    Added component structure

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Item() {
+  return (
+    <div>
+        <h1>
+            Item en venta. 
+        </h1>
+    </div>
+  )
+}
---
+++ src/components/ItemCart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ItemCart() {
+  return (
+    <div>
+        <h1>Soy un item de carrito</h1>
+    </div>
+  )
+}
