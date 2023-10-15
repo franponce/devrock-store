7ad6b6fe32c475ca67d2ad2da1157fd46edc048e
El diego <franponce98@gmail.com>
2023-10-15 20:18:23 -0300

    Routes and layout are added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { children } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-export default function Layout() {
+export default function Layout(props) {
+  const {children} = props;
   return (
-    <div>
-
-    </div>
+    <>
+    <Header></Header>
+    {children}
+    <Footer></Footer>
+    </>
   )
 }
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from '../components/Layout';
+import Home from '../containers/Home';
+import Cart from '../containers/Cart';
+import Products from '../containers/Products';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
   crossorigin="anonymous"
 />
-      <h1>Dev Store</h1>
-    </div>
+
+<BrowserRouter>
+<Layout>
+<Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/carrito' element={<Cart />} />
+          <Route path='/productos' element={<Products />} />
+</Routes>
+</Layout>
+</BrowserRouter>
+    </>
   );
 }
+
 
 export default App;
